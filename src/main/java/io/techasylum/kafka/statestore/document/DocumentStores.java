f0eb1429c11fa50941eb0b589c76f5a259bb4c2a
Tim Ysewyn <tysewyn@korfinancial.com>
2022-03-28 17:39:23 +0200

    Removed Nitrite object state store

--- src/main/java/io/techasylum/kafka/statestore/document/DocumentStores.java
+++ src/main/java/io/techasylum/kafka/statestore/document/DocumentStores.java
@@ -1,7 +1,6 @@
 package io.techasylum.kafka.statestore.document;
 
 import io.techasylum.kafka.statestore.document.no2.NitriteDocumentStoreBuilder;
-import io.techasylum.kafka.statestore.document.no2.NitriteObjectStore;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.streams.state.StoreBuilder;
 import org.dizitart.no2.Document;
@@ -10,11 +9,6 @@ public final class DocumentStores {
 
     public static <K> NitriteDocumentStoreBuilder<K> nitriteStore(String name, Serde<K> keySerde, Serde<Document> valueSerde, String keyFieldName) {
         return new NitriteDocumentStoreBuilder<>(name, keySerde, valueSerde, keyFieldName);
-    }
-
-    public static <K, V> StoreBuilder<NitriteObjectStore<K, V>> nitriteObjectStore(String name, Serde<K> keySerde, Serde<V> valueSerde, Class<V> valueClass, String keyFieldName) {
-        throw new UnsupportedOperationException("Object store note yet supported");
-//        return new NitriteObjectStoreBuilder<>(name, keySerde, valueSerde, valueClass, keyFieldName);
     }
 
 }
